d2054fc847406b353e21b9f4f2048227254b975d
Octoboros <Team.Octoboros@gmail.com>
2023-01-02 01:10:13 -0500

    Further development of basic knowledge - Basic Blocks

--- src/main/java/net/team_octoboros/testing_mod/Testing_Mod.java
+++ src/main/java/net/team_octoboros/testing_mod/Testing_Mod.java
@@ -14,6 +14,7 @@ import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.team_octoboros.testing_mod.block.ModBlocks;
 import net.team_octoboros.testing_mod.item.ModItems;
 import org.slf4j.Logger;
 
@@ -32,6 +33,7 @@ public class Testing_Mod
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(eventBus);
+        ModBlocks.register(eventBus);
 
         eventBus.addListener(this::setup);
 
